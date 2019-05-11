4fda7ceb04517c0b2d8aa89c184e189f835bc23e
Javier Goldschmidt <javi@elgatoylacaja.com>
2019-05-11 15:20:59 -0300

    Funcionamiento Basico de Nucleos

--- Qarth Game/Assets/Scripts/QarthNode.cs
+++ Qarth Game/Assets/Scripts/QarthNode.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class QarthNode : MonoBehaviour
 {
-
+    public Renderer rend;
+    public Color originalColor;
+    Material mat;
+    List<Wall> walls;
+    public bool inUse;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend.material.color = originalColor;
+        mat = rend.material;
+        walls = new List<Wall>();
     }
 
     // Update is called once per frame
@@ -18,4 +24,27 @@ public class QarthNode : MonoBehaviour
         
     }
 
+    public void Selected(Player player)
+    {
+        if (walls.Count < 1) mat.color = player.playerColor;
+    }
+
+    public void Deselect()
+    {
+        if (walls.Count < 1) mat.color = originalColor;
+    }
+
+    public QarthNode WallConnect(Wall wall)
+    {
+        if (walls.Count < 1) mat.color = wall.parent.playerColor;
+        walls.Add(wall);
+        return this;
+    }
+
+    public QarthNode WallDisconnect(Wall wall)
+    {
+        walls.Remove(wall);
+        if (walls.Count < 1) mat.color = originalColor;
+        return this;
+    }
 }
