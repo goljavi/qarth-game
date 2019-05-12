2d712e1319db8f48d2e6ffbf24899c3fd7f2a09f
Chiqquii <47392436+Chiqquii@users.noreply.github.com>
2019-05-12 14:58:06 -0300

    algo

--- Qarth Game/Assets/GameManager.cs
+++ Qarth Game/Assets/GameManager.cs
@@ -16,8 +16,8 @@ public class GameManager : MonoBehaviour
     float timeElapsed;
 
     bool spawnearSuccionadores;
+    public bool finishLevel;
 
-    // Update is called once per frame
     void Update()
     {
         if (!stopSpawn)
